c3997b1b07e5131c78cb6f523db9887eac168cfd
Rodrigo Morais <58960796+rodigu@users.noreply.github.com>
2021-12-22 00:54:05 -0300

    Added to Extra Functions
    Reading and Writing separate for exchange rates and currency list
    Function for getting complete exchange list and currencies

--- extraFunctions.ts
+++ extraFunctions.ts
@@ -1,30 +1,45 @@
 // Functions for writing and reading the list of possible exchanges between currencies
 export async function writeExchangeList (list:any) {
-  await Deno.writeTextFile('./validExchangeList.txt', JSON.stringify(list));
+  await Deno.writeTextFile('./ExchangeList.txt', JSON.stringify(list));
 }
 
-export async function getExchangeList () {
-  return [...await JSON.parse(await Deno.readTextFile('./validExchangeList.txt'))];
+export async function readExchangeList () {
+  return await JSON.parse(await Deno.readTextFile('./ExchangeList.txt'));
 }
 
-export async function getCompleteExchangeList (exchangeRates, info) {
+export async function writeCurrencyList (list:any) {
+  await Deno.writeTextFile('./CurrencyList.txt', JSON.stringify(list));
+}
+
+export async function readCurrencyList () {
+  return [...await JSON.parse(await Deno.readTextFile('./ExchangeList.txt'))];
+}
+
+export function getCompleteExchangeList (exchangeRates:Array<{symbol:string,price:string}>, info:any) {
+  // returns all necessary information to build a complete graph
   let { symbols } = info;
-  let completeList = symbols.map(e=>{
+  let completeList = symbols.map((e:any)=>{
     return {symbol: e.symbol, base: e.baseAsset, quote: e.quoteAsset}
   });
 
+  // get all possible currencies into a set
+  let currencies = new Set(completeList.map((e:any)=>e.base));
+  console.log(currencies)
+  // add any currencies that might only be showing as quotes
+  currencies.add(new Set(completeList.map((e:any)=>e.quote)));
+
   // pair exchange rates with symbols
-  let priceMap = {};
+  let priceMap:Map<string,string> = new Map();
   for (let trade of exchangeRates){
     let {symbol,price} = trade;
-    priceMap[symbol] = price;
+    priceMap.set(symbol, price);
   }
 
   completeList.forEach(
-    element=>{
-      element.price = priceMap[element.symbol];
+    (element:any)=>{
+      element.price = priceMap.get(element.symbol);
     }
   );
 
-  return completeList;
+  return { exchangeList: completeList, currencies:[...currencies] };
 }
