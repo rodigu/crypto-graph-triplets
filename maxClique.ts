88fc74fc66781f51b1abf9869b421dd176da08a2
Rodrigo Morais <58960796+rodigu@users.noreply.github.com>
2022-01-19 16:17:36 -0300

    Types

--- maxClique.ts
+++ maxClique.ts
@@ -1,11 +1,16 @@
-import { ExchangeList, Exchange, getNeighbors } from './extraFunctions.ts';
+import { getNeighbors } from './extraFunctions.ts';
+import { ExchangeList } from './enum.ts';
+/**
+ * Takes in a list of currencies that migh make a clique, and the complete network which contains these currencies.
+ * @param  {string[]} currency_list
+ * @param  {ExchangeList} exchange_list
+ * @returns boolean
+ */
+export function isClique (currency_list:string[], exchange_list:ExchangeList) : boolean {
 
-export function isClique (currency_list:Array<string>, exchange_list:ExchangeList) : Boolean {
-  //  Takes in a list of currencies that migh make a clique, and the complete
-  // network which contains these currencies
   let result = true
   currency_list.forEach(currency => {
-    let neighbors = getNeighbors(currency, exchange_list);
+    const neighbors = getNeighbors(currency, exchange_list);
     currency_list.forEach(c=>{
       if (c !== currency && neighbors.indexOf(c)===-1){
         result = false;
@@ -17,17 +22,17 @@ export function isClique (currency_list:Array<string>, exchange_list:ExchangeLis
   return result;
 }
 
-export function largerSubGraph (graphA:Array<string>, graphB:Array<string>):Array<string> {
-  let larger_graph = Math.max(graphA.length, graphB.length) === graphA.length ? graphA : graphB;
+export function largerSubGraph (graphA:string[], graphB:string[]) : string[] {
+  const larger_graph = Math.max(graphA.length, graphB.length) === graphA.length ? graphA : graphB;
   return larger_graph;
 }
 
-export function maxClique (currency_list:Array<string>,exchange_list:ExchangeList, min_neighbors:Array<string>) : Array<string> {
+export function maxClique (currency_list:string[],exchange_list:ExchangeList, min_neighbors:string[]) : string[] {
   // min_neighbors
-  let max_clique:Array<string> = [...min_neighbors];
+  let max_clique:string[] = [...min_neighbors];
   currency_list.forEach((currency) => {
     if (max_clique.indexOf(currency) > -1) return;
-    let temp_max_clique = [currency, ...max_clique];
+    const temp_max_clique = [currency, ...max_clique];
     console.log(max_clique)
     if (isClique(temp_max_clique, exchange_list)) {
       max_clique = largerSubGraph(temp_max_clique, max_clique);
