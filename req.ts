6226e2b1f3f0a49b2b98d020f4ec9037b03a0dbc
Rodrigo Morais <58960796+rodigu@users.noreply.github.com>
2022-03-01 12:38:18 -0800

    Typing, comments, time limits

--- req.ts
+++ req.ts
@@ -1,33 +1,35 @@
-async function fetchExchange(currency: string) {
+import { APIInfo, APIExchangeRates } from "./enum.ts";
+
+/**
+ * Returns a promise with the API's price object
+ * @param  {string} currency="all"
+ * @returns Promise<APIExchangeRates>
+ */
+export async function getExchangeRate(
+  currency = "all"
+): Promise<APIExchangeRates> {
   let link = `https://api.binance.us/api/v3/ticker/price`;
   currency === "all" ? 1 : (link += `?symbol=${currency}`);
-  try {
-    const response = await fetch(link);
-    const data = await response.json();
-    return data;
-  } catch (error) {
-    console.error(error);
-  }
-}
-
-async function renderData(currency: string) {
-  const data = await fetchExchange(currency);
+  const response = await fetch(link);
+  const data = await response.json();
   return data;
 }
 
-// gets exchange rate between two or all currencies
-export async function getExchangeRate(currency = "all") {
-  return await renderData(currency);
+export let LAST = new Date().getTime();
+
+function sleep(millis = 1000) {
+  return new Promise((resolve) => setTimeout(resolve, millis));
 }
 
-// gets info for all possible currency trades
-export async function getExchangeInfo() {
+/**
+ * Returns a promise with the API's exchangeInfo object
+ * @returns Promise<APIInfo>
+ */
+export async function getExchangeInfo(): Promise<APIInfo> {
   const link = "https://api.binance.us/api/v3/exchangeInfo";
-  try {
-    const response = await fetch(link);
-    const data = await response.json();
-    return data;
-  } catch (error) {
-    console.error(error);
-  }
+  const response = await fetch(link);
+  const data = await response.json();
+  await sleep(1000 - (new Date().getTime() - LAST));
+  LAST = new Date().getTime();
+  return data;
 }
