9efd5d30ddc503fd1c8d6463cbf735d1d4009e11
Rodrigo Morais <58960796+rodigu@users.noreply.github.com>
2022-03-03 09:41:08 -0800

    Fixes to graphData

--- data_vis.ts
+++ data_vis.ts
@@ -8,10 +8,10 @@ import { createCanvas } from "https://deno.land/x/canvas/mod.ts";
 export async function graphData(
   data: number[],
   file: string,
-  mag = 200,
   min_mag = 0,
-  n_div = { width: 10, height: 10 },
-  size: { width: number; height: number } = { width: 300, height: 200 }
+  max_mag = 20,
+  n_div = { width: 5, height: 5 },
+  size: { width: number; height: number } = { width: 1000, height: 800 }
 ) {
   const { width, height } = size;
   const canv = createCanvas(width, height);
@@ -23,8 +23,8 @@ export async function graphData(
   ctx.fillRect(0, 0, width, height);
 
   const step_size = width / data.length;
-  ctx.lineWidth = 2;
-  ctx.fillStyle = line_color;
+  ctx.lineWidth = 0.5;
+  ctx.fillStyle = "gray";
   ctx.lineCap = "round";
 
   const div_step_width = size.width / n_div.width;
@@ -33,12 +33,14 @@ export async function graphData(
     ctx.beginPath();
     ctx.moveTo(i * div_step_width, 0);
     ctx.lineTo(i * div_step_width, height);
+    ctx.stroke();
   }
 
   for (let i = 0; i < n_div.height; i++) {
     ctx.beginPath();
     ctx.moveTo(0, i * div_step_height);
     ctx.lineTo(width, i * div_step_height);
+    ctx.stroke();
   }
 
   const convert = (
@@ -49,11 +51,15 @@ export async function graphData(
     min_b: number
   ) => (val - min_v) * ((max_b - min_b) / (max_v - min_v)) + min_b;
 
+  ctx.lineWidth = 2;
+  ctx.fillStyle = line_color;
+  ctx.lineCap = "round";
+
   let prev_y = height;
   data.forEach((num, index) => {
     ctx.beginPath();
     ctx.moveTo(index * step_size, prev_y);
-    prev_y = convert(num, mag, min_mag, 0, height);
+    prev_y = convert(num, max_mag, min_mag, 0, height);
     ctx.lineTo((index + 1) * step_size, prev_y);
     ctx.stroke();
   });
